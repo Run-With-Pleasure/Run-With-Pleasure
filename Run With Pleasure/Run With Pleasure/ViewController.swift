f5f7197980d888c162cfd7f6ceabea40826e1cd0
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-06-30 11:14:18 +0800

    Initial

---
+++ Run With Pleasure/Run With Pleasure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Run With Pleasure
+//
+//  Created by Александр on 30.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
