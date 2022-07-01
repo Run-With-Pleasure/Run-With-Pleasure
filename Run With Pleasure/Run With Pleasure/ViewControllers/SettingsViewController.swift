a14dc6c3fb3b6a390b493a2eb1c04502b69fc1be
Viacheslav Turchak <wachek7@gmail.com>
2022-07-01 22:59:05 +0300

    setup text fields logic

--- Run With Pleasure/Run With Pleasure/ViewControllers/SettingsViewController.swift
+++ Run With Pleasure/Run With Pleasure/ViewControllers/SettingsViewController.swift
@@ -19,7 +19,10 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        nameTextField.delegate = self
+        lastNameTextField.delegate = self
+        ageTextField.delegate = self
+        sexTextField.delegate = self
     }
     
     
@@ -40,4 +43,70 @@ class SettingsViewController: UIViewController {
     }
     */
 
+}
+
+// MARK: - Text Fields
+
+extension SettingsViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let newValue = textField.text else { return }
+        
+        if textField == nameTextField {
+            if Float(newValue) != nil {
+                showAlert(with: "Incorrect value", and: "Please, enter your name")
+                nameTextField.text = ""
+                return
+            } else {
+                person.name = newValue
+            }
+        } else if textField == lastNameTextField {
+            if Float(newValue) != nil {
+                showAlert(with: "Incorrect value", and: "Please, enter your lastname")
+                lastNameTextField.text = ""
+                return
+            } else {
+                person.lastname = newValue
+            }
+        } else if textField == ageTextField {
+            guard let numberValue = Float(newValue), numberValue > 0 else {
+                showAlert(with: "Incorrect value", and: "Please, enter your age")
+                ageTextField.text = ""
+                return
+            }
+            person.age = Int(newValue) ?? 0
+        } else {
+            if Float(newValue) != nil {
+                showAlert(with: "Incorrect value", and: "Please, enter your sex")
+                sexTextField.text = ""
+                return
+            } else {
+                person.sex = newValue
+            }
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
+    }
+}
+
+// MARK: - Alert Controller
+
+extension SettingsViewController {
+    
+    private func showAlert(with title: String, and massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let returnAction = UIAlertAction(title: "Return", style: .cancel) { _ in
+        }
+        alert.addAction(returnAction)
+        present(alert, animated: true)
+    }
+    
 }
